d88e677eb7d98a67e956eec1c2fa68cd85991b88
Ohyaelim <npr05080@gmail.com>
2021-05-18 13:59:13 +0900

    testmongo 삭제

--- backend/src/main/java/sm/chromeScreentime/testmongo/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package sm.chromeScreentime.testmongo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer,String> {
-}
