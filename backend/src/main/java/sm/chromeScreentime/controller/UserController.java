f11dce705ad7326ea57faa42547da1f69ac6dbf4
Ohyaelim <npr05080@gmail.com>
2021-05-29 07:43:45 +0900

    controller - api 틀잡기 시작

--- backend/src/main/java/sm/chromeScreentime/controller/UserController.java
+++ backend/src/main/java/sm/chromeScreentime/controller/UserController.java
@@ -1,16 +1,38 @@
 package sm.chromeScreentime.controller;
 
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sm.chromeScreentime.Service.UserService;
+import sm.chromeScreentime.model.UrlEntity;
+import sm.chromeScreentime.model.UserEntity;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/classification")
 public class UserController {
-    private final UserService userService;
+    @Autowired
+    UserService userService;
+    //create -post
+    //read -get
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+
+    public List<UserEntity> findByEmail(String email){
+        return userService.findByEmail(email);
     }
 
+    public UrlEntity findByUrl(ObjectId url){
+        return userService.findByUrl(url);
+    }
+
+    public List<UrlEntity> findByDomainLike(String domain){
+        return userService.findByDomainLike(domain);
+    }
+
+    public List<UrlEntity> findByCategory(String category){
+        return userService.findByCategory(category);
+    }
 }
