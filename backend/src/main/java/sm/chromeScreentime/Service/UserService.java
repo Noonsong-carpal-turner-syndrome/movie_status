a2ab40bce64ef588bfb03b643fa6c0b9902ceb60
Ohyaelim <npr05080@gmail.com>
2021-05-29 07:30:53 +0900

    UserService 채워넣기 (repo기반)

--- backend/src/main/java/sm/chromeScreentime/Service/UserService.java
+++ backend/src/main/java/sm/chromeScreentime/Service/UserService.java
@@ -1,19 +1,37 @@
 package sm.chromeScreentime.Service;
 
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sm.chromeScreentime.model.UrlEntity;
+import sm.chromeScreentime.model.UserEntity;
 import sm.chromeScreentime.repository.UrlRepository;
 import sm.chromeScreentime.repository.UserRepository;
 
+import java.util.List;
+
+@Service
 public class UserService {
-    private UserRepository userRepository;
-    private UrlRepository urlRepository;
 
-    public UserService(UserRepository userRepository){
-        this.userRepository = userRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UrlRepository urlRepository;
+
+    public List<UserEntity> findByEmail(String email){
+        return userRepository.findByEmail(email);
     }
 
-    public UserService(UrlRepository urlRepository){
-        this.urlRepository = urlRepository;
+    public UrlEntity findByUrl(ObjectId url){
+        return urlRepository.findByUrl(url);
     }
 
+    public List<UrlEntity> findByDomainLike(String domain){
+        return urlRepository.findByDomainLike(domain);
+    }
 
+    public List<UrlEntity> findByCategory(String category){
+        return urlRepository.findByCategory(category);
+    }
 }
