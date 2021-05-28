028a038b06d18630438e524564a7f8e77b20f7f7
Ohyaelim <npr05080@gmail.com>
2021-05-29 03:10:04 +0900

    UserService 재정비 시작

--- backend/src/main/java/sm/chromeScreentime/Service/UserService.java
+++ backend/src/main/java/sm/chromeScreentime/Service/UserService.java
@@ -1,7 +1,19 @@
 package sm.chromeScreentime.Service;
 
+import sm.chromeScreentime.repository.UrlRepository;
 import sm.chromeScreentime.repository.UserRepository;
 
 public class UserService {
+    private UserRepository userRepository;
+    private UrlRepository urlRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public UserService(UrlRepository urlRepository){
+        this.urlRepository = urlRepository;
+    }
+
 
 }
--- backend/src/main/java/sm/chromeScreentime/controller/UserController.java
+++ backend/src/main/java/sm/chromeScreentime/controller/UserController.java
@@ -1,7 +1,17 @@
 package sm.chromeScreentime.controller;
 
-//Restful API 만들기
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sm.chromeScreentime.Service.UserService;
+
+@RestController
+@RequestMapping
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
 
 }
