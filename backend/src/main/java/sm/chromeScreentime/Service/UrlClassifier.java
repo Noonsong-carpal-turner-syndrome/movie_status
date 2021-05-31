400db8d99b2e14d112362b6a1d71dc4cdf1f0cbc
randallkk <randallkk@sookmyung.ac.kr>
2021-05-31 11:37:28 +0900

    210531 Flask Api: classification

--- backend/src/main/java/sm/chromeScreentime/Service/UrlClassifier.java
+++ backend/src/main/java/sm/chromeScreentime/Service/UrlClassifier.java
@@ -1,2 +1,46 @@
-package sm.chromeScreentime.Service;public class UrlClassifier {
+package sm.chromeScreentime.Service;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class UrlClassifier {
+    @RequestMapping(value = "/test.do", method = RequestMethod.GET)
+    public String Classify() {
+        String url = "http://127.0.0.1:5000/tospring";
+        String sb = "";
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                sb = sb + line + "\n";
+            }
+            System.out.println("========br======" + sb.toString());
+
+            if (sb.toString().contains("ok")) {
+                System.out.println("test");
+            }
+            br.close();
+
+            System.out.println("" + sb.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return sb.toString();
+    }
 }
